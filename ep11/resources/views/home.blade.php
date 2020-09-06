1938d0bec93ff29b5ab345eaca7ddecde8ed0898
Joao Henrique <joao.gh61@gmail.com>
2020-09-06 18:59:31 -0300

    UTILIZE LARAVEL COM BOOTSTRAP DO JEITO CERTO

---
+++ ep11/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('site/style.css')}} ">
+    <title>Laravel - Bootstrap</title>
+</head>
+<body>
+<h1>Laravel - Bootstrap</h1>
+<button class="btn btn-lg btn-orange">BootStrap OK</button>
+<script src="{{asset('site/jquery.js')}} "></script>
+<script src="{{asset('site/bootstrap.js')}} "></script>
+</body>
+</html>
